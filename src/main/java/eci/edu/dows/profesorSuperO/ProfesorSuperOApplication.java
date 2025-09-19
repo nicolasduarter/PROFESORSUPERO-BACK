85b2cc0bdaa076257f1c1a71099f857591d55360
Nicolas Duarte <nicolas.duarte-r@mail.escuelaing.edu.cogit satatusgit statusgit commit -m finish>
2025-09-19 11:33:58 -0500

    estructura y pom

---
+++ src/main/java/eci/edu/dows/profesorSuperO/ProfesorSuperOApplication.java
@@ -0,0 +1,13 @@
+package eci.edu.dows.profesorSuperO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfesorSuperOApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfesorSuperOApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eci/edu/dows/profesorSuperO/ProfesorSuperOApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.dows.profesorSuperO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfesorSuperOApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
