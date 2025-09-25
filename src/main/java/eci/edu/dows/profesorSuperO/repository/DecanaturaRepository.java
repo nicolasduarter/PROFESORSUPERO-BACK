4e4160fe6c7c760434924c964d95c64df47dc443
juanfe-rangel <rangeljuanfe687@gmail.com>
2025-09-25 18:57:51 -0500

    servicios solicitud y repositorio decanatura

---
+++ src/main/java/eci/edu/dows/profesorSuperO/repository/DecanaturaRepository.java
@@ -0,0 +1,12 @@
+package eci.edu.dows.profesorSuperO.repository;
+
+import eci.edu.dows.profesorSuperO.model.Decanatura;
+import eci.edu.dows.profesorSuperO.model.Facultades;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface DecanaturaRepository extends MongoRepository<Decanatura, String> {
+    Optional<Decanatura> findByFacultad(Facultades facultad);
+}
+
