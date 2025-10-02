234f4e4464741d4e86f8c0bee0a1d391b944181c
juanfe-rangel <rangeljuanfe687@gmail.com>
2025-10-01 22:46:00 -0500

    proyecto - modificacion solicitudes dtos

--- src/main/java/eci/edu/dows/profesorSuperO/model/DTOS/SolicitudesDTO/SolicitudCambioMateriaDTO.java
+++ src/main/java/eci/edu/dows/profesorSuperO/model/DTOS/SolicitudesDTO/SolicitudCambioMateriaDTO.java
@@ -1,27 +1,28 @@
 package eci.edu.dows.profesorSuperO.model.DTOS.SolicitudesDTO;
 
-import eci.edu.dows.profesorSuperO.model.Estudiante;
-import eci.edu.dows.profesorSuperO.model.Grupo;
-import eci.edu.dows.profesorSuperO.model.Materia;
+import eci.edu.dows.profesorSuperO.model.DTOS.GrupoDTO;
+import eci.edu.dows.profesorSuperO.model.DTOS.MateriaDTO;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDate;
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class SolicitudCambioMateriaDTO extends SolicitudDTO {
-    @NotNull(message = "Materria no puede ser vacia")
-    private Materia materiaProblema;
-    @NotNull(message = "Materria nueva no puede ser vacia")
-    private Materia materiaCambio;
-    @NotNull(message = "El  grupo  no puede ser vacia")
-    private Grupo grupo;
-    @NotNull(message = "El nuevo grupo  no puede ser vacia")
-    private Grupo grupoCambio;
 
+    @NotNull(message = "La materia no puede ser vacía")
+    private MateriaDTO materiaProblema;
+
+    @NotNull(message = "La nueva materia no puede ser vacía")
+    private MateriaDTO materiaCambio;
+
+    @NotNull(message = "El grupo no puede ser vacío")
+    private GrupoDTO grupo;
+
+    @NotNull(message = "El nuevo grupo no puede ser vacío")
+    private GrupoDTO grupoCambio;
 }
